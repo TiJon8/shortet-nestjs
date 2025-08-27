8460579aa785526325ff761939e32b69d9b44e02
tishka <alex.gromov2@mail.ru>
2025-08-27 06:09:53 +0300

    test(LinkController): added tests covering api linkController

--- src/features/link/tests/link.controller.spec.ts
+++ src/features/link/tests/link.controller.spec.ts
@@ -1,10 +1,22 @@
 import { Test, TestingModule } from "@nestjs/testing"
 import { LinkController } from "../link.controller"
 import { LinkService } from "../link.service"
+import { NotFoundException } from "@nestjs/common"
+
+const mockUserId = {
+	id: 'AaAa-1111-BbBb-2222'
+}
 
 const mockLinkService = {
-	create: jest.fn().mockResolvedValue({ url: 'https://example.com/aabbccddee' }),
-	delete: jest.fn().mockResolvedValue(true)
+
+	create: jest.fn().mockImplementation((data: { url: string }, id: string) => {
+		if (id !== mockUserId.id) throw new NotFoundException('Invalid request')
+		return { url: 'https://example.com/aabbccddee' }
+	}),
+	delete: jest.fn().mockImplementation((id: string) => {
+		if (id !== mockUserId.id) throw new NotFoundException()
+		return true
+	})
 }
 
 describe('LinkController', () => {
@@ -32,9 +44,36 @@ describe('LinkController', () => {
 
 	describe('api controller testing', () => {
 		test('should create a shorter link', async () => {
-			const result = await linkController.create({ original: 'https://example.com/video?w=11' }, '1')
-			expect(linkService.create).toHaveBeenCalledWith('https://example.com/video?w=11', '1')
+			const result = await linkController.create({ original: 'https://example.com/video?w=11' }, mockUserId.id)
+			expect(linkService.create).toHaveBeenCalledWith('https://example.com/video?w=11', mockUserId.id)
 			expect(result).toEqual({ url: `https://example.com/aabbccddee` })
+		})
+
+		test('should throw an NotFounfException if userId is not valid or is not exists', async () => {
+			jest.spyOn(linkController, 'create').mockRejectedValue(new NotFoundException())
+
+			try {
+				await linkController.create({ original: 'https://example.com/video?w=11' }, mockUserId.id)
+			} catch (e) {
+				expect(e).toBeInstanceOf(NotFoundException)
+			}
+
+		})
+
+		test('should delete a link', async () => {
+			const result = await linkController.delete(mockUserId.id)
+			expect(result).toBe(true)
+		})
+
+		test('should throw an NotFounfException', async () => {
+			jest.spyOn(linkController, 'delete').mockRejectedValue(new NotFoundException())
+
+			try {
+				await linkController.delete('1')
+			} catch (e) {
+				expect(e).toBeInstanceOf(NotFoundException)
+			}
+
 		})
 	})
 
